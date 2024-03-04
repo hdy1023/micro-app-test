ce791434766a901033920574faac85ee036bdda3
胡丁尧 <hudingyao@vrvmail.com.cn>
2024-03-04 14:30:20 +0800

    1.0.0-rc.4问题整理完善。

--- main/src/layout/index.vue
+++ main/src/layout/index.vue
@@ -5,7 +5,7 @@
         <router-link v-for="(item,index) in navList" :key="index" :to="item.path" style="margin-right:20px;">{{item.name}}</router-link>
       </p>
       <p>
-        <span>子应用跳转：<button v-for="(item,index) in appNavList" :key="index" @click="handleJump(item)" style="margin-right:20px;">{{item.name}}</button></span>
+        <span>主应用控制子应用跳转：<button v-for="(item,index) in appNavList" :key="index" @click="handleJump(item)" style="margin-right:20px;">{{item.name}}</button></span>
       </p>
     </div>
     <div>
@@ -66,9 +66,30 @@ export default {
 
   },
   methods: {
+    microJump (item) {
+      /* 1.0.0-rc.4 */
+      if (config.native) {
+        microApp.router.push({ name: item.appName, path: routerBase + item.path })
+      } else {
+        //虚拟路由开启下，子应用window.__MICRO_APP_BASE_ROUTE__为空，因此子应用路由不受baseroute配置影响（即便baseroute有值）
+        microApp.router.push({ name: item.appName, path: item.path })
+      }
+    },
     handleJump (item) {
-      console.log('click menu item:',item)
-      this.$router.push(item.path)
+      console.log('click menu item:', item)
+      /* 1.0.0-rc.3 */
+      /* this.$router.push(item.path) */
+
+      /* 1.0.0-rc.4 */
+      if (this.$route.name === item.appName) {
+        this.microJump(item)
+      } else {
+        if (config.native) {
+          this.$router.push(item.path)
+        } else {
+          this.$router.push(`/${item.appName}?${item.appName}=${item.path}`)
+        }
+      }
     },
   }
 }
--- main/src/views/subApp.vue
+++ main/src/views/subApp.vue
@@ -24,9 +24,10 @@ export default {
     handleJump () {
       microApp.router.push({ name: this.name, path: this.path })
     },
-    beforeshow(){
-      this.$nextTick(()=>{
-        window.dispatchEvent(new PopStateEvent('popstate', { state: history.state }))
+    beforeshow () {
+      this.$nextTick(() => {
+        /* 1.0.0-rc.3 */
+        /* window.dispatchEvent(new PopStateEvent('popstate', { state: history.state })) */
         /* microApp.router.push({ name: this.name, path: routerBase + this.$route.path }) */
       })
     },
@@ -70,11 +71,27 @@ export default {
       优先使用PopStateEvent，可以触发主应用路由watch，保证逻辑统一。microApp.router关闭虚拟路由系统下无法触发主应用路由watch。
     */
 
+    /* 1.0.0-rc.4 发现问题（以下问题均为关闭虚拟路由系统）*/
+    /* 
+      问题：
+      1.microApp.router 在虚拟路由系统下会触发主应用路由watch，关闭虚拟路由系统不触发主应用watch。(此问题在1.0.0-rc.3也存在)
+      2.microApp.router 如果当前url无变化，microApp.router不会生效，比如子应用挂载后，先通过主应用路由让url变为对应子应用页面url，再调用microApp.router子应用页面不更新。（此问题在1.0.0-rc.3不存在，可以使用PopStateEvent替代microApp.router，但是官方推荐的子应用调整方式是microApp.router）
+      3.因为问题2，强行使用microApp.router，需要更改点击子应用跳转的逻辑，但又因为microApp.router在当前子应用未挂载时无法调用（会报错），所以导致跳转子应用时需要先判断该子应用是否正在运行，如果未运行，则需要调用vue.router.push，反之才能调用microApp.router.push。没有之前watch route + PopStateEvent 便捷清晰。
+      4.开启keep-alive时，首次进入microApp.router可以控制子应用跳转。但是子应用失活后重新激活后，microApp.router和PopStateEvent都无法控制子应用跳转了，尝试了很多方法均无效，这个问题是致命的导致无法升级。（此问题在1.0.0-rc.3中未发现，且开启虚拟路由系统仍然存在）
+
+      期望：
+      1.开启keep-alive时，子应用再次激活后能够正常控制跳转。
+      2.microApp.router 在各种配置场景下效果无差异，关闭虚拟路由系统时触发主应用路由watch。
+      3.microApp.router 能够像1.0.0-rc.3一样，支持在浏览器url未变化情况下触发子应用页面更新。
+      4.microApp.router 做为PopStateEvent替代，希望能够兼容PopStateEvent的效果。
+      5.microApp.router 能够在当前子应用未挂载时也可以调用，如未挂载主动挂载并完成对应子应用页面跳转，避免采取vue.router+microApp.router的冗余方式。
+    */
   },
   watch: {
     $route (val) {
       console.log(val, '主应用监听到路由change')
-      window.dispatchEvent(new PopStateEvent('popstate', { state: history.state }))
+      /* 1.0.0-rc.3 */
+      /* window.dispatchEvent(new PopStateEvent('popstate', { state: history.state })) */
       /* microApp.router.push({ name: this.name, path: routerBase + val.path }) */
     },
   }
